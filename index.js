f44b8690f94dede505bb5026a7a4862b55fc2597
MELODY SIMWA <simwamelody@gmail.com>
2024-11-28 18:42:50 +0300

    createindex.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const receivesAFunction = (callback)=>{
+    callback();
+}
+
+const returnsANamedFunction = () => {
+    return function namedFunction() {
+      console.log("I am a named function!");
+    };
+  };
+  
+  
+  const returnsAnAnonymousFunction = () => {
+    return function () {
+      console.log("I am an anonymous function!");
+    };
+  };
+  
